55115d1c35a7aa293f0afc35309592900fa1b7e3
fabulosa <jiangwj1992@gmail.com>
2018-12-16 23:41:04 -0800

    complete files for train

---
+++ train/data_process.py
@@ -0,0 +1,161 @@
+__author__ = 'jwj'
+import pickle
+import numpy as np
+
+
+"""
+# training data: 2008 Fall to 2016 Fall
+# test data: 2017 Spring
+# subtraining data: 2008 Fall to 2015 Fall
+# validation data: 2016 Spring
+"""
+
+def empty(check):
+    if check[0] == {}:
+        return 0
+    else:
+        return 1
+
+
+def get_data_from_condense_seq(t):  # t==22, subtraining and validation; t==25, training and testing
+    f = open('../../RNN2/data_preprocess/stu_sem_major_grade_condense.pkl', 'rb')
+    data = pickle.load(f)['stu_sem_major_grade_condense']
+    data = np.array(data)
+    train = data[:, :t]
+    vali = data[:, :t+1]
+    f.close()
+    num_stu = train.shape[0]
+    num_sem = np.zeros(num_stu)
+    num_sem1 = np.zeros(num_stu)
+
+    for i in range(num_stu):
+        nonempty_sem = np.apply_along_axis(empty, 0, train[i][np.newaxis, :])
+        num_sem[i] = np.sum(nonempty_sem)
+        nonempty_sem = np.apply_along_axis(empty, 0, vali[i][np.newaxis, :])
+        num_sem1[i] = np.sum(nonempty_sem)
+
+    # filter training data, periods >=2
+    index = np.where(num_sem >= 2)[0]
+    training_data = train[index]
+
+    # filter validation data, periods >=2
+    index = np.where(num_sem1 >= 2)[0]
+    validation_data = vali[index]
+    # filter validation data again, the last semester shouldn't be null
+    vali = validation_data[:, t]
+    nonempty_vali = np.apply_along_axis(empty, 0, vali[np.newaxis, :])
+    index = np.where(nonempty_vali > 0)[0]
+    validation_data = validation_data[index][:, :t+1]
+    print("Number of sub-training samples: ", len(training_data))
+    print("Number of validation samples: ", len(validation_data))
+    return training_data, validation_data
+
+
+# padding
+# input: x(batch_size, sem_num, [course_id_t+1, course_id_grade_id], output: x(batch_size, sem_num, [course_id_grade_id]
+def process_data(index, data, batchsize, dim_input_course, dim_input_grade, dim_input_major):
+    batch = data[index]
+    num_sem = np.zeros(batchsize, int)
+    for i in range(batchsize):
+        nonempty_sem = np.apply_along_axis(empty, 0, batch[i][np.newaxis, :])
+        num_sem[i] = np.sum(nonempty_sem)
+    sort_index = np.argsort(-num_sem)
+    max_seq = num_sem.max()
+    pro_grade = np.zeros((batchsize, int(max_seq), dim_input_grade), int)  # padded input
+    pro_course = np.zeros((batchsize, int(max_seq), dim_input_course), int)  # padded input
+    pro_major = np.zeros((batchsize, int(max_seq), dim_input_major), int)  # padded input
+    input_padded = np.zeros((batchsize, int(max_seq), dim_input_course + dim_input_grade + dim_input_major), int)  # padded input
+    label_padded = np.zeros((batchsize, int(max_seq)-1, dim_input_grade), int)  # padded label
+    stu_flag = 0
+    for j in sort_index:
+        sem_flag = 0
+        for k in batch[j]:
+            if k != {}:
+                pro_major[stu_flag, sem_flag, k['major']] = 1
+                for s in k['grade']:
+                    if s[1] <= 2:
+                        pro_grade[stu_flag, sem_flag, s[0] * 4] = 1
+                    elif s[1] <= 5:
+                        pro_grade[stu_flag, sem_flag, s[0] * 4 + 1] = 1
+                    elif s[1] == 6:
+                        pro_grade[stu_flag, sem_flag, s[0] * 4 + 2] = 1
+                    elif s[1] == 7:
+                        pro_grade[stu_flag, sem_flag, s[0] * 4 + 3] = 1
+                    pro_course[stu_flag, sem_flag, s[0]] = 1
+                sem_flag += 1
+        label_padded[stu_flag] = pro_grade[stu_flag, 1:]
+        input_padded[stu_flag, :, dim_input_course:dim_input_course+dim_input_grade] = pro_grade[stu_flag]
+        input_padded[stu_flag, :-1, :dim_input_course] = pro_course[stu_flag, 1:]
+        input_padded[stu_flag, :, -dim_input_major:] = pro_major[stu_flag]
+        input_padded[stu_flag, sem_flag-1] = 0
+        stu_flag += 1
+
+    # cut 0 vectors for the second dimension of input_padded.
+    input_padded = input_padded[:, :-1]
+
+    label_len = np.sum(label_padded, axis=2)
+    label_sem = np.zeros((label_len.shape[0], label_len.shape[1]), int)
+    label_sem[np.nonzero(label_len)] = 1
+    label_length = np.sum(label_sem, axis=1)
+
+    input_len = np.sum(input_padded, axis=2)
+    input_sem = np.zeros((input_len.shape[0], input_len.shape[1]), int)
+    input_sem[np.nonzero(input_len)] = 1
+    input_length = np.sum(input_sem, axis=1)
+
+    if not (label_length == input_length).all():
+        print("label length or input length wrong! wrong padding!")
+        exit()
+    return input_padded, input_length, label_padded
+
+
+# pad batch data x(batch_size, sem_num, [course_id, grade_id]) to x(batch_size, max_seq, course_num*7)
+def process_data_cat_continuous(index, data, batchsize, dim_input):
+
+    # dict for mapping discrete grade to continuous grade
+    grade_dic = {1: 5, 2: 4, 3: 3, 4: 2, 5: 1, 6: 2, 7: 1}  # discrete: continuous
+    batch = data[index]
+    num_sem = np.zeros(batchsize, int)
+    for i in range(batchsize):
+        nonempty_sem = np.apply_along_axis(empty, 0, batch[i][np.newaxis, :])
+        num_sem[i] = np.sum(nonempty_sem)
+    sort_index = np.argsort(-num_sem)
+    max_seq = num_sem.max()
+    pro_input_data = np.zeros((batchsize, int(max_seq), dim_input), int)  # padded input
+    pro_label_data = np.zeros((batchsize, int(max_seq), int(dim_input / 7)), int)
+    input_padded = np.zeros((batchsize, int(max_seq), dim_input), int)  # padded input
+    label_padded = np.zeros((batchsize, int(max_seq)-1, int(dim_input / 7)), int)  # padded label
+    stu_flag = 0
+    for j in sort_index:
+        sem_flag = 0
+        for k in batch[j]:
+            if k != []:
+                for s in k:
+                    pro_input_data[stu_flag, sem_flag, s[0] * 7 - 1 + s[1]] = 1
+                    pro_label_data[stu_flag, sem_flag, s[0]] = grade_dic[s[1]]
+                sem_flag += 1
+        label_padded[stu_flag] = pro_label_data[stu_flag, 1:]
+        input_padded[stu_flag] = pro_input_data[stu_flag]
+        input_padded[stu_flag, sem_flag-1] = 0
+        stu_flag += 1
+
+    # cut 0 vectors for the second dimension of input_padded.
+    input_padded = input_padded[:, :-1]
+
+    label_len = np.sum(label_padded, axis=2)
+    label_sem = np.zeros((label_len.shape[0], label_len.shape[1]), int)
+    label_sem[np.nonzero(label_len)] = 1
+    label_length = np.sum(label_sem, axis=1)
+
+    input_len = np.sum(input_padded, axis=2)
+    input_sem = np.zeros((input_len.shape[0], input_len.shape[1]), int)
+    input_sem[np.nonzero(input_len)] = 1
+    input_length = np.sum(input_sem, axis=1)
+
+    if not (label_length == input_length).all():
+        print("label length or input length wrong! wrong padding!")
+        exit()
+    #print(input_length, label_length)
+
+    return input_padded, input_length, label_padded
+
---
+++ train/evaluate_cat_cat.py
@@ -0,0 +1,168 @@
+__author__ = 'jwj'
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn import functional as F
+import myLSTM as LSTM
+import pickle
+import numpy as np
+import torch.utils.data as Data
+from data_process import process_data, get_data_from_condense_seq
+from metrics import accuracy, sensitivity
+
+
+# for validation loss in early stopping
+def evaluate_loss(model, loader, vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major, weight1, weight2):
+
+    model.eval()
+    summ = []
+    for step, (batch_x, batch_y) in enumerate(loader):  # batch_x: index of batch data
+        processed_data = process_data(batch_x.numpy(), vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major)
+        padded_input = Variable(torch.Tensor(processed_data[0]), requires_grad=False).cuda()
+        seq_len = processed_data[1]
+        padded_label = Variable(torch.Tensor(processed_data[2]), requires_grad=False).cuda()
+
+        # clear hidden states
+        model.hidden = model.init_hidden()
+        model.hidden[0] = model.hidden[0].cuda()
+        model.hidden[1] = model.hidden[1].cuda()
+        # compute output
+        y_pred = model(padded_input, seq_len).cuda()
+        # only compute the loss for testing period
+        loss = model.vali_loss(y_pred, seq_len, padded_label, weight1, weight2).cuda()
+        summ.append(loss.data[0])
+    average_loss = np.average(summ)
+    return average_loss
+
+
+# for validation accuracy in early stopping
+def evaluate_accuracy(model, loader, vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major):
+
+    model.eval()
+    summ1 = 0  # ABCDF
+    summ2 = 0  # credit/uncredit
+    len1 = len2 = 0
+    for step, (batch_x, batch_y) in enumerate(loader):  # batch_x: index of batch data
+        processed_data = process_data(batch_x.numpy(), vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major)
+        padded_input = Variable(torch.Tensor(processed_data[0]), requires_grad=False).cuda()
+        seq_len = processed_data[1]
+        padded_label = Variable(torch.Tensor(processed_data[2]), requires_grad=False).cuda()
+
+        # clear hidden states
+        model.hidden = model.init_hidden()
+
+        # compute output
+        y_pred = model(padded_input, seq_len).cuda()
+
+        # only compute the accuracy for testing period
+        accura = accuracy(y_pred, seq_len, padded_label)
+        len1 += accura[3]
+        len2 += accura[4]
+        summ1 += (accura[0] * accura[3])
+        summ2 += (accura[1] * accura[4])
+
+    average_accuracy = (summ1 + summ2) / (len1 + len2)
+    return average_accuracy
+
+
+# for validation
+def evaluate_metrics(model, loader, vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major):
+
+    model.eval()
+    summ1 = 0  # >=B or <B
+    summ2 = 0  # credit/uncredit
+
+    len1 = len2 = 0
+    tp = np.zeros(2)
+    tn = np.zeros(2)
+    true = np.zeros(2)
+    false = np.zeros(2)
+    predict_true = np.zeros(2)
+    predict_false = np.zeros(2)
+    for step, (batch_x, batch_y) in enumerate(loader):  # batch_x: index of batch data
+        processed_data = process_data(batch_x.numpy(), vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major)
+        padded_input = Variable(torch.Tensor(processed_data[0]), requires_grad=False).cuda()
+        seq_len = processed_data[1]
+        padded_label = Variable(torch.Tensor(processed_data[2]), requires_grad=False).cuda()
+
+        # clear hidden states
+        model.hidden = model.init_hidden()
+        model.hidden[0] = model.hidden[0].cuda()
+        model.hidden[1] = model.hidden[1].cuda()
+        # compute output
+        y_pred = model(padded_input, seq_len)
+
+        # only compute the accuracy for testing period
+        accura = accuracy(y_pred, seq_len, padded_label)
+        len1 += accura[3]
+        len2 += accura[4]
+        summ1 += (accura[0] * accura[3])
+        summ2 += (accura[1] * accura[4])
+
+        print('>=B or not', accura[0], 'credit/uncredit', accura[1], 'total', accura[2])
+
+        # compute tp, fp, fn, tn
+        sen = sensitivity(y_pred, seq_len, padded_label)
+        tp += sen[0]
+        tn += sen[1]
+        true += sen[2]
+        false += sen[3]
+        predict_true += sen[4]
+        predict_false += sen[5]
+
+    average_metric1 = summ1 / len1
+    average_metric2 = summ2 / len2
+    average_metric = (summ1 + summ2) / (len1 + len2)
+
+    print("num of >=B or <B: ", len1, "num of credit/uncredit: ", len2)
+    print("On average: ", average_metric1, average_metric2, average_metric)
+
+    tpr = tp / true
+    fpr = (predict_true - tp) / false
+    fnr = (predict_false - tn) / true
+    tnr = tn / false
+
+    precision_B = (tn / predict_false)[0]
+    f_value_B = 2 / (1 / tnr[0] + 1 / precision_B)
+    precision_uncredit = (tn / predict_false)[-1]
+    f_value_uncredit = 2 / (1 / tnr[-1] + 1 / precision_uncredit)
+    f_value = np.append(f_value_B, f_value_uncredit)
+    print("tpr: ", tpr)
+    print("fpr: ", fpr)
+    print("fnr: ", fnr)
+    print("tnr: ", tnr)
+    print('F: ', f_value, 'average F:', np.average(f_value))
+
+if __name__ == '__main__':
+
+    batchsize = 32
+
+    # evaluate model on the test set
+    model_name = 'nw_LSTM_cat_cat_1_50lr0.001drp0.2wd1e-07clp0.pkl'
+    model = torch.load(model_name)
+
+    # validation or testign
+    vali_or_test = 'vali'
+
+    # subtrain or train
+    if vali_or_test == 'vali':
+        time = 22
+    else:
+        time = 25
+
+    f = open('../../RNN/data_preprocess/course_id.pkl', 'rb')
+    course_id = pickle.load(f)['course_id']
+    f = open('../../RNN2/data_preprocess/major_id.pkl', 'rb')
+    major_id = pickle.load(f)['major_id']
+    dim_input_course = len(course_id)
+    dim_input_grade = len(course_id) * 4
+    dim_input_major = len(major_id)
+    f.close()
+
+    vali_data = get_data_from_condense_seq(time)[1]
+    vali_data_index = torch.IntTensor(np.array(range(vali_data.shape[0])))
+    torch_vali_data_index = Data.TensorDataset(data_tensor=vali_data_index, target_tensor=vali_data_index)
+    vali_loader = Data.DataLoader(dataset=torch_vali_data_index, batch_size=batchsize, shuffle=True, num_workers=2, drop_last=True)
+
+    evaluate_metrics(model, vali_loader, vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major)
+    print(model_name)
