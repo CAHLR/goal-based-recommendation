cbd16384cfeacd00d652853229d6e3481d89b2a6
fabulosa <jiangwj1992@gmail.com>
2021-01-11 01:34:24 -0800

    update trainng files

--- train/evaluate.py
+++ train/evaluate.py
@@ -9,6 +9,7 @@ import numpy as np
 import torch.utils.data as Data
 from data_process import process_data, get_data_from_condense_seq
 from metrics import accuracy, sensitivity
+from utils import *
 
 
 # for validation loss in early stopping
@@ -31,7 +32,7 @@ def evaluate_loss(model, loader, vali_data, batchsize, dim_input_course, dim_inp
         # only compute the loss for testing period
         loss = model.vali_loss(y_pred, seq_len, padded_label).cuda()
         summ.append(loss.item())
-        
+
     average_loss = np.average(summ)
     return average_loss
 
@@ -70,7 +71,7 @@ def evaluate_metrics(model, loader, vali_data, batchsize, dim_input_course, dim_
         summ1 += (accura[0] * accura[3])
         summ2 += (accura[1] * accura[4])
 
-        print('>=cutoff or not', accura[0], 'credit/uncredit', accura[1], 'total', accura[2])
+        print('accuracy on letter grades', accura[0], 'accuracy on credit/uncredit', accura[1], 'total', accura[2])
 
         # compute tp, fp, fn, tn
         sen = sensitivity(y_pred, seq_len, padded_label)
@@ -80,13 +81,13 @@ def evaluate_metrics(model, loader, vali_data, batchsize, dim_input_course, dim_
         false += sen[3]
         predict_true += sen[4]
         predict_false += sen[5]
-
+        
     average_metric1 = summ1 / len1
     average_metric2 = summ2 / len2
     average_metric = (summ1 + summ2) / (len1 + len2)
 
-    print("num of >=cutoff or <cutoff: ", len1, "num of credit/uncredit: ", len2)
-    print("On average: ", average_metric1, average_metric2, average_metric)
+    print("number of letter grades ", len1, "num of credit/no credit: ", len2)
+    print("Accuracy--", "letter grades:", average_metric1, "credit/no credit", average_metric2, "overall", average_metric)
 
     tpr = tp / true
     fpr = (predict_true - tp) / false
@@ -98,42 +99,36 @@ def evaluate_metrics(model, loader, vali_data, batchsize, dim_input_course, dim_
     precision_uncredit = (tn / predict_false)[-1]
     f_value_uncredit = 2 / (1 / tnr[-1] + 1 / precision_uncredit)
     f_value = np.append(f_value_B, f_value_uncredit)
+    print("Further evaluation on prediction of letter grade and credit/no credit: ", "[on letter grade, on credit/no credit]")
     print("tpr: ", tpr)
     print("fpr: ", fpr)
     print("fnr: ", fnr)
     print("tnr: ", tnr)
     print('F: ', f_value, 'average F:', np.average(f_value))
 
+
 if __name__ == '__main__':
-
-    batchsize = 32
-
-    # evaluate model on the test set
-    model_name = 'nw_LSTM_cat_cat_1_50lr0.001drp0.2wd1e-07clp0.pkl'
+    args = parse_args()
+    batchsize = args.batch_size
+    model_name = args.evaluated_model_path
     model = torch.load(model_name)
 
-    # validation or testign
-    vali_or_test = 'vali'
+    evaluated_semester = args.evaluated_semester
 
-    # subtrain or train
-    if vali_or_test == 'vali':
-        time = 22
-    else:
-        time = 25
-
-    f = open('../../RNN/data_preprocess/course_id.pkl', 'rb')
+    f = open(args.course_id_path, 'rb')
     course_id = pickle.load(f)['course_id']
-    f = open('../../RNN2/data_preprocess/major_id.pkl', 'rb')
+    f = open(args.major_id_path, 'rb')
     major_id = pickle.load(f)['major_id']
+    f.close()
     dim_input_course = len(course_id)
     dim_input_grade = len(course_id) * 4
     dim_input_major = len(major_id)
-    f.close()
 
-    vali_data = get_data_from_condense_seq(time)[1]
+    vali_data = get_data_from_condense_seq(evaluated_semester)[1]
     vali_data_index = torch.IntTensor(np.array(range(vali_data.shape[0])))
-    torch_vali_data_index = Data.TensorDataset(data_tensor=vali_data_index, target_tensor=vali_data_index)
+    torch_vali_data_index = Data.TensorDataset(vali_data_index, vali_data_index)
     vali_loader = Data.DataLoader(dataset=torch_vali_data_index, batch_size=batchsize, shuffle=True, num_workers=2, drop_last=True)
 
+    print('Evaluation results of ', model_name)
     evaluate_metrics(model, vali_loader, vali_data, batchsize, dim_input_course, dim_input_grade, dim_input_major)
-    print(model_name)
+
